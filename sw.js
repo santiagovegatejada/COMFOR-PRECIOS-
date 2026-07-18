576ca338bb15847c3c49b0d98eb4e9a7dcf86be2
santiagovegatejada <67612028+santiagovegatejada@users.noreply.github.com>
2026-07-18 13:23:31 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker: deja la app disponible sin internet
-const CACHE = 'comfort-precios-v3';
+const CACHE = 'comfort-precios-v6';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-180.png'];
 
 self.addEventListener('install', e => {
